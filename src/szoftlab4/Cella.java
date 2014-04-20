b5856f4de8d9ab98dd0b3b8628818453d9f2d5a9
gyufasdoboz <csonkalorant@gmail.com>
2014-04-20 23:38:30 +0200

    Lóránt része
    A mező közelbenvan, és az út lépnék fv-e bibis

--- src/szoftlab4/Cella.java
+++ src/szoftlab4/Cella.java
@@ -3,67 +3,55 @@ package szoftlab4;
 import java.util.ArrayList;
 
 public class Cella {
-	private ArrayList<Cella> szomszedok;	// Lista a szomszéd cellákról
+	protected ArrayList<Cella> szomszedok;	// Lista a szomszéd cellákról
+	private Boolean boolean1;
 	
+	public Cella()
+	{
+		
+	}
 	
-	public Cella(){		// A Cella konstruktora
-		szomszedok = new ArrayList<Cella>();
-		szomszedok.add(Tesztesetek.testUt);
+	public Cella(ArrayList<Cella> l){		// A Cella konstruktora
+		szomszedok = l;
 	}
 	
 	public void ratesz(Mezorevalo elem) // Cellára tesz mezõrevaló elemet (itt nem használt)
 	{
-		Log.log(LogType.CALL, this, "ratesz_mezore");
 		
-		Log.log(LogType.RETURN, null, "void");
 	}
 	
 	public void ratesz(Utravalo elem) // Cellára tesz Útravaló elemet (itt nem használt)
 	{
-		Log.log(LogType.CALL, this, "ratesz_utra");
 		
-		Log.log(LogType.RETURN, null, "void");
 	}
 	
 	public void levesz(Mezorevalo elem) // Cellról levesz mezõrevaló elemet (itt nem használt)
 	{
-		Log.log(LogType.CALL, this, "levesz");
 		
-		Log.log(LogType.RETURN, null, "levesz");
 	}
 	
 	public void levesz(Utravalo elem) // Cellról levesz útravaló elemet (itt nem használt)
 	{
-		Log.log(LogType.CALL, this, "levesz");
 		
-		Log.log(LogType.RETURN, null, "levesz");
 	}
 	
 
 	
 	public ArrayList<Ellenseg> kivanrajtam() // Visszaad egy Ellenséglistát azokról az ellenségekrõl akik az adott cellán(úton) vannak (itt nem használt)
 	{
-		Log.log(LogType.CALL, this, "kivanrajtam");
-		
-		Log.log(LogType.RETURN, null, "ArrayList<Ellenseg>");
 		return null;
 	}
 	
 	public ArrayList<Cella> getSzomszedok() // Visszaadja a szomszéd cellák listáját
 	{
-		Log.log(LogType.CALL, this, "getSzomszedok");
-		
-		Log.log(LogType.RETURN, null, "ArrayList<Cella>");
 		return szomszedok;
 	}
 	
 
 	public boolean mezovagyok() // igazzal tér vissza, ha mezõ, hamissal ha út (itt nem használt)
 	{
-		Log.log(LogType.CALL, this, "mezovagyok");
-		Log.log(LogType.RETURN, null, "boolean");
-		return (Boolean) null;
-		
+		boolean1 = (Boolean) null;
+		return boolean1;		
 	}
 	
 
--- src/szoftlab4/Ut.java
+++ src/szoftlab4/Ut.java
@@ -8,48 +8,32 @@ public class Ut extends Cella{
 	public Akadaly akadaly;
 	ArrayList<Cella> szomszedok;
 	
-	public Ut(){
+	public Ut(ArrayList<Cella> l){
 		rajtamvan = new ArrayList<Utravalo>();
-		szomszedok = new ArrayList<Cella>();
+		szomszedok = l;
 	}
 	
 	public void ratesz(Utravalo elem)				//Cellára tesz Útravaló elemet 
 	{	
-		Log.log(LogType.CALL, this, "ratesz_ut");
 		rajtamvan.add(elem);
-		Tesztesetek.testUt.levesz(elem);
-		Tesztesetek.testEllenseg.SajatUtCsere(this);
-		
 		elem.init(this);
-		
-		Log.log(LogType.RETURN, null, "void");
 	}
 	
 	public void levesz(Utravalo elem)				// Cellról levesz mezõrevaló elemet 
 	{
-		Log.log(LogType.CALL, this, "levesz_ut");
 		rajtamvan.remove(elem);
-		Log.log(LogType.RETURN, null, "void");
 	}
 	
 	public boolean mezovagyok()						// Igazzal tér vissza, ha mezõ, hamissal ha út 	
 	{
-		Log.log(LogType.CALL, this, "mezovagyok_ut");
-		Log.log(LogType.RETURN, null, "boolean");
-		return false;
+		return false;								// Mert bizony szegény Cella nem Mezõ, hanem Út :'( 
 	}
 	
 	public ArrayList<Ut> lepnek()					// A közelben lévõ utak listájával tér vissza
 	{
-		Log.log(LogType.CALL, this, "lepnek_ut");
-		ArrayList<Ut> temp_ut = null;
-		for(int i=0;i<this.szomszedok.size();i++)
-		{
-			if(!this.szomszedok.get(i).mezovagyok())
-				temp_ut.add((Ut) this.szomszedok.get(i));
-		}
-		Log.log(LogType.RETURN, null, "ArrayList<Ut>");
-		return temp_ut;
+		ArrayList<Ut> temp = null;
+		// TODO: Ugyan az a helyzet mint a Mezo osztály közelbenvan fv-ével... a  szomszédok listája Cellákat tárol, így nem lehet Ut-ra szûrni.. :(
+		return temp;
 	}
 
 }
